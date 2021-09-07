628a8fd610a4d52e8317da6532a933a6bf4fee62
Andor <andorhorvat@gmail.com>
2021-09-07 13:39:06 +0200

    Download done.

--- system/response/downloadResponse/DownloadResponse.php
+++ system/response/downloadResponse/DownloadResponse.php
@@ -4,13 +4,39 @@ namespace System\response\downloadResponse;
 
 class DownloadResponse
 {
+    private array $dataToDownload;
+
     public function __construct(array $dataToDownload)
     {
-
+        $this->dataToDownload = $dataToDownload;
     }
 
-    public static function send()
+    
+    public function sendResponse()
     {
-        
+        //https://code.iamkate.com/php/creating-downloadable-csv-files/
+        // output headers so that the file is downloaded rather than displayed
+        header('Content-Type: text/csv; charset=utf-8');
+        header('Content-Disposition: attachment; filename=data.csv');
+        http_response_code(200);
+        // create a file pointer connected to the output stream. So we create the csv file in the output, so it does not need a storage directory
+        $output = fopen('php://output', 'w');
+
+        // output the column headings
+        // fputcsv($output, ['Category', 'Cost']);
+
+
+        foreach ($this->getDataToDownload() as $category => $cost) {
+            $line = [$category, $cost];
+            fputcsv($output, $line);
+        }
+    }
+
+    /**
+     * Get the value of dataToDownload
+     */ 
+    public function getDataToDownload(): array
+    {
+        return $this->dataToDownload;
     }
 }
