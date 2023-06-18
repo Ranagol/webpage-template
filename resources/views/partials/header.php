ca2c3d8a6c85f5095127be0396438be1da816319
Andor <andorhorvat@gmail.com>
2023-06-18 20:35:09 +0200

    Favicon - css - consol errors corrected.

--- resources/views/partials/header.php
+++ resources/views/partials/header.php
@@ -2,8 +2,10 @@
 <html>
 <head>
 	<title>*webpage*</title>
+	<!-- This below is a solution for the 'favicon.ico not found -->
+	<link rel="shortcut icon" href="#">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="/resources/css/style.css">
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
 </head>
 <body>
 	<?php require 'navbar.php';?>
