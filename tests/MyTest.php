08349d3bb8a50fef74740772252c13e2f141970f
Ranagol <andorhorvat@gmail.com>
2024-10-31 22:19:23 +0100

    Install PHPUnit.

---
+++ tests/MyTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;//Every test class must be a child of TestCase class for testing
+
+class MyTest extends TestCase {
+
+    public function testFirstAssertion(){
+        $this->assertTrue(true);
+    }
+
+}
