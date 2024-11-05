e91bc3a18a3ff9bcdf917ba9ef0837607ca06291
Ranagol <andorhorvat@gmail.com>
2024-11-05 14:24:36 +0100

    Create TrainCommand.

---
+++ app/console/TrainCommand.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\console;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use App\trains\TrainService;
+
+class TrainCommand extends Command
+{
+    // Configure the command
+    protected static $defaultName = 'trains';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Solves the trains problem')
+            ->setHelp('This command allows you to solve the trains problem');
+    }
+
+    // Execute the command
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $trainService = new TrainService();
+
+        $output->writeln('Command was executed, see the STDOUT file for the output');
+        return Command::SUCCESS;
+    }
+}
--- console.php
+++ console.php
@@ -3,12 +3,14 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
 use App\console\HelloCommand;
+use App\console\TrainCommand;
 
 // Create a new Symfony Console application
 $application = new Application();
 
-// Register commands here
+// This is where we register our custom commands. 
 $application->add(new HelloCommand());
+$application->add(new TrainCommand());
 
 // Run the console application
 $application->run();
