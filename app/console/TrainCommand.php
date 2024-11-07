88f20f6f80046c0fd055726546aa97b6f0b5466a
Ranagol <andorhorvat@gmail.com>
2024-11-07 19:04:02 +0100

    Add memory and time measuring.

--- app/console/TrainCommand.php
+++ app/console/TrainCommand.php
@@ -22,16 +22,21 @@ class TrainCommand extends Command
     /**
      * Execute the command
      * 
+     * Start the terminal in docker container: 
+     * docker-compose exec -it php-container bash
+     * 
+     * Run the app and input the train timetable:
      * cat app/trains/trainTimetable | php console.php trains
      * 
-     * docker-compose exec -it php-container bash
-     *
      * @param InputInterface $input
      * @param OutputInterface $output
      * @return void
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        // Start measuring the execution time
+        $startTime = microtime(true);
+
         //this will get everything, the whole data from STDIN
         $trainTimetable = stream_get_contents(STDIN);
 
@@ -42,10 +47,31 @@ class TrainCommand extends Command
 
         // var_dump($trainTimetable);//This will show us all the data from the file
 
-        $trainService = new TrainService($trainTimetable);
-        $trainService->handle();
+        // $trainService = new TrainService($trainTimetable);
+        // $trainService->handle();
+
+        /**
+         * Here we measure the max memory usage
+         */
+        // Get peak memory usage
+        $peakMemoryUsage = memory_get_peak_usage(true); // true for real memory usage
+        // Convert peak memory usage to a human-readable format
+        $peakMemoryUsage = number_format($peakMemoryUsage / 1048576, 2) . ' MB';
+        $output->writeln("Peak memory usage: $peakMemoryUsage");
+
+        /**
+         * Here we have the logic for execution time measuring and displaying in cli
+         */
+        // End measuring the execution time
+        $endTime = microtime(true);
+        $executionTime = ($endTime - $startTime);
+        // Format the elapsed time to 6 decimal places
+        $executionTime = number_format($executionTime, 6);
+        $output->writeln("Execution time: $executionTime seconds");
+
 
         $output->writeln('Command was executed!');
         return Command::SUCCESS;
     }
 }
+
