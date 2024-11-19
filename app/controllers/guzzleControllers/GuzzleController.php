a5a6488f4a40513a00c30788d63dc9daa5dd57ff
Ranagol <andorhorvat@gmail.com>
2024-11-19 18:50:15 +0100

    Guzzle refactored.

--- app/controllers/guzzleControllers/GuzzleController.php
+++ app/controllers/guzzleControllers/GuzzleController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controllers\GuzzleControllers;
 
+use App\controllers\Controller;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Request;
 
 /**
- * The GuzzleController class is meant for sending guzzle requests to https://dummyapi.io/.
+ * The GuzzleController class is meant for sending guzzle requests to https://dummyapi.io/. Also for
+ * displaying the received data.
  */
-class GuzzleController
+class GuzzleController extends Controller
 {
     /**
      * This is the base uri for the dummyApi page. 
@@ -16,7 +18,7 @@ class GuzzleController
      *
      * @var string
      */
-    private static $baseUri = 'https://dummyapi.io/data/v1/';
+    private $baseUri = 'https://dummyapi.io/data/v1/';
 
     /**
      * This is the additional uri that we need to make a get request regarding the posts.
@@ -27,23 +29,23 @@ class GuzzleController
      *
      * @var string
      */
-    private static $postsUri = 'post?page=0&limit=10';
+    private $postsUri = 'post?page=0&limit=10';
 
     /**
      * It is required to set app-id Header for each request.
      *
      * @var array
      */
-    private static $headers = ['app-id' => '612de3a4265d8631fe1d0028'];
+    private $headers = ['app-id' => '612de3a4265d8631fe1d0028'];
 
     /**
      * Loads guzzle page
      *
      * @return void
      */
-    public static function loadGuzzlePage(): void
+    public function loadGuzzlePage(): void
     {
-        returnView('guzzle');
+        $this->view('guzzle');
     }
 
     /**
@@ -53,14 +55,14 @@ class GuzzleController
      *
      * @return void
      */
-    public static function getPosts(): void
+    public function getPosts(): void
     {   
         /**
          * This ['verify' => false] is a must, without this there will be some SSL error.
          */
         $client = new Client(['verify' => false]);
-        $url = self::$baseUri . self::$postsUri;
-        $headers = self::getHeaders();
+        $url = $this->baseUri . $this->postsUri;
+        $headers = $this->getHeaders();
         $request = new Request(
             'GET', 
             $url,
@@ -72,37 +74,46 @@ class GuzzleController
             /**
              * The request is send, and the response is received here.
              */
-            $t = 8;
             $response = $client->send($request);//itt van a probléma
-            $t = 8;
-
             $response = json_decode($response->getBody(), true);
             $posts = $response['data'];
-            // var_dump($posts['data']);
-            
-            returnView('guzzle', compact('posts'));
+
+            $this->view('guzzle', ['posts' => $posts]);
             
         } catch (\Throwable $error) {
-            // var_dump('GUZZLE ERROR');
             echo 'My error message: ' . $error->getMessage() . '<br>';
             echo 'The error was triggered in this file: ' . $error->getFile(). '<br>';
             echo 'The error was triggered on this line: ' . $error->getLine(). '<br>';
-
         }
     }
 
-    public static function getHeaders(): array
+    /**
+     * Returns the headers.
+     *
+     * @return array
+     */
+    private function getHeaders(): array
     {
-        return self::$headers;
+        return $this->headers;
     }
     
-    public static function getPostsUri(): string
+    /**
+     * Returns the posts uri.
+     *
+     * @return string
+     */
+    private function getPostsUri(): string
     {
-        return self::$postsUri;
+        return $this->postsUri;
     }
-   
-    public static function getBaseUri(): string
+    
+    /**
+     * Returns the base uri.
+     *
+     * @return string
+     */
+    private function getBaseUri(): string
     {
-        return self::$baseUri;
+        return $this->baseUri;
     }
 }
---
+++ resources/views/guzzle.blade.php
@@ -0,0 +1,33 @@
+@extends('layout')
+
+@section('content')
+
+<h1>Guzzle testing page</h1>
+
+<p>We are sending here requests with Guzzle to the 
+    <a href="https://dummyapi.io/">DummyApi</a> page.
+</p>
+
+<div class='container d-flex flex-row'>
+    <form action="getPosts" method='GET'>
+        <button class='btn btn-success'>GET 10 posts from DummyApi</button>
+    </form>
+
+    <form action="guzzle" method='GET'>
+        <button class='btn btn-success'>Reset</button>
+    </form>
+</div>
+
+<div class='mt-5'>
+    <h3 class='mt-5'>Received responses</h3>
+    <p> Note: the received data is on purpose displayed with just a var_dump, since the
+        goal here was only to get some data from a webpage with the help of the Guzzle, and not
+        to do a frontend exercise.
+    </p>
+
+    @if(isset($posts))
+        {{ var_dump($posts) }}
+    @endif
+</div>
+
+@endsection
