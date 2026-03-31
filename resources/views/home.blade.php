45ac886426238f3beabd6e587d2f3a56a4f3b3e7
Andor Horvat <andorhorvat@gmail.com>
2026-03-31 06:46:47 +0200

    Add 4 links to home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,14 +17,28 @@
 <section class="feature-grid" aria-label="Project Challenges">
     
     <article class="feature-card" style="text-align:justify">
-        <h3><i class="bi bi-code-slash"></i> Challenge 1: Raw PHP MVC Application</h3>
+        <h3>
+            <a href="/raw-php-mvc"
+                class="text-decoration-underline"
+            >
+                <i class="bi bi-code-slash"></i> 
+                Challenge 1: Raw PHP MVC Application
+            </a>
+        </h3>
         <p>
             A custom MVC web application built with vanilla PHP, without using any frameworks. 
         </p>
     </article>
 
     <article class="feature-card" style="text-align:justify">
-        <h3><i class="bi bi-upload"></i> Challenge 2: File Upload & Processing</h3>
+        <h3>
+            <a href="/upload"
+                class="text-decoration-underline"
+            >
+                <i class="bi bi-upload"></i> 
+                Challenge 2: File Upload & Processing
+            </a>
+        </h3>
         <p>
             A custom file upload and download system built in raw PHP. The app will process the
             uploaded CSV file, perform calculations, and generate a downloadable report.
@@ -32,7 +46,14 @@
     </article>
 
     <article class="feature-card" style="text-align:justify">
-        <h3><i class="bi bi-train-front"></i> Challenge 3: Train Schedule Calculator</h3>
+        <h3>
+            <a href="/train-task"
+                class="text-decoration-underline"
+            >
+                <i class="bi bi-train-front"></i> 
+                Challenge 3: Train Schedule Calculator
+            </a>
+        </h3>
         <p>
             A scheduling problem that calculates train timetables based on given inputs. The task is 
             executed via the command line, and results are displayed directly in the console output.
@@ -40,7 +61,14 @@
     </article>
 
     <article class="feature-card" style="text-align:justify">
-        <h3><i class="bi bi-shield-shaded"></i>Challenge 4: Heroes and Monsters</h3>
+        <h3>
+            <a href="/heroes-and-monsters"
+                class="text-decoration-underline"
+            >
+                <i class="bi bi-shield-shaded"></i> 
+                Challenge 4: Heroes and Monsters
+            </a>
+        </h3>
         <p>
             A fantasy simulation inspired by role-playing games like Advanced Dungeons & Dragons. 
             The system models a world of heroes and monsters using object-oriented PHP. Characters 
