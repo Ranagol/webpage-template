a117e027158c36979a0bbffdb8d02defafbfcd48
Andor Horvat <andorhorvat@gmail.com>
2026-04-05 23:01:19 +0200

    Stop tracking public/cache directory

--- public/cache/d7fbeada8492961e54cce85e111c3c6f513744f1.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <!-- <title><?php echo e($title); ?></title> -->
-</head>
-<body>
-    <?php echo $__env->yieldContent('content'); ?>
-</body>
-</html><?php /**PATH /srv/www/resources/views/layout.blade.php ENDPATH**/ ?>
